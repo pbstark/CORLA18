52e843669bb1b87c276e50cd2f2d13f17e39195a
Vanessa Teague <vjteague@unimelb.edu.au@4180l-137754-l.mobility.unimelb.net.au>
2019-10-21 10:14:39 +1100

    Added file for some utils.

---
+++ code/IRVVisualisationUtils.py
@@ -0,0 +1,15 @@
+# Convert a tree in list form into the same tree in tuple form suitable for
+# svgling.
+def treeListToTuple(t):
+    # If t is an empty list, we shouldn't have got this far.
+    if not t:
+        print("Error: empty list in tree drawing")
+    # Leaf.  Return the name of the candidate and the assertion we've excluded it with.
+    if len(t) == 1:     
+        return((t[0][0],t[0][1]+str(t[0][2]))) 
+    # Otherwise recurse.
+    else:
+        tList = []
+        for branch in t[1]:
+            tList.append(treeListToTuple(branch))
+        return ((t[0],)+tuple(tList))
