b4c2c4c5c50b3b76938b456a6f3c697e665be86a
vanessa <vgit@greatcactus.org>
2019-10-21 22:23:15 +1100

    Printing.

--- code/IRVVisualisationUtils.py
+++ code/IRVVisualisationUtils.py
@@ -100,7 +100,7 @@ def printAssertions(WOLosers,IRVElims):
     
     print("Not-Eliminated-Next assertions: ")
     for winner in IRVElims:
-        print('NEN {0:4d}: Candidate {1:2d} cannot be eliminated next when '+str(winner[1])+' are eliminated.'.format(IRVElims.index(winner),winner[0]))
+        print('NEN {0:4d}: Candidate {1:2d} cannot be eliminated next when '.format(IRVElims.index(winner),winner[0]+str(winner[1])+' are eliminated.'))
 
 
 # Build printable pretty trees.
